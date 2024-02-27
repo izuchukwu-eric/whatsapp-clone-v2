a532013babd34e7cc00edaea6d843c5c36476d80
izuchukwu-eric <onukwubeizu@gmail.com>
2024-02-27 03:28:31 +0100

    setup done

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Page = () => {
+  return (
+    <View>
+        <Text>
+            Index page
+        </Text>
+    </View>
+  )
+}
+
+export default Page
